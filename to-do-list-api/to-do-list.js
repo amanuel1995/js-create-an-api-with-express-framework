0fe625b9d84a9e91bcc039d48479113ade90b3f4
Amanuel <amaniye1@live.com>
2017-04-26 05:37:40 -0400

    module exports api for to do list

---
+++ to-do-list-api/to-do-list.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'CSE-410': {
+    ToDoTitle: '410 Project 10 Submission.',
+    Done: 'NO'
+  },
+  'CSE-422': {
+    ToDoTitle: 'CSE 422 Honors Research Paper Submission.',
+    Done: 'YES'
+  },
+  'MI-449': {
+    ToDoTitle: 'Complete All MI 449 Projects.',
+    Done: 'YES'
+  },
+  'Online Surveys': {
+    ToDoTitle: 'Fill Out SIRS Forms.',
+    Done: 'YES'
+  },
+  'Finals': {
+    ToDoTitle: 'Start Preparing For Final Exams.',
+    Done: 'NO'
+  }
+}
